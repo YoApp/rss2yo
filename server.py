092d584cd647490510f2317a2b5c65d30c8b2d4b
michael perret <michael.t.perret@gmail.com>
2015-01-14 16:34:10 -0800

    attempting to fix port binding issues

--- server.py
+++ server.py
@@ -194,7 +194,7 @@ app = web.Application([
 ], debug=True)
 
 if __name__ == '__main__':
-    app.listen(9001)
+    app.listen(int(os.environ.get('PORT', '5000')))
     crawlRSS()
     ioloop.IOLoop.instance().start()
 
